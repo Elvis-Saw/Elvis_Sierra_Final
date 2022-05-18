7d127e21cbafb83dce0c406da48bd3b6700f9802
steven.cardona <steven.cardona@globant.com>
2022-05-18 16:32:31 -0500

    create new test cases

--- src/test/java/tests/CategoryListTest.java
+++ src/test/java/tests/CategoryListTest.java
@@ -4,10 +4,11 @@ import org.springframework.context.annotation.Description;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import screens.DashBoardScreen;
-import screens.MapScreen;
+import screens.mapScreen.MapScreen;
+import screens.mapScreen.widgets.CategoriesWidget;
 import util.tests.BaseMobileTest;
 
-public class NavigationTest extends BaseMobileTest {
+public class CategoryListTest extends BaseMobileTest {
 
     /**
      * Validate Map Screen Navigation.
@@ -17,16 +18,24 @@ public class NavigationTest extends BaseMobileTest {
     public void navigateToMapScreen() {
         log.info("Start Navigation to Map Screen");
         DashBoardScreen dashBoard = loadDashBoardScreen();
+
+        dashBoard.closeUpdateMessage();
+
+        log.info("Validate Map Button is Displayed");
+        Assert.assertTrue(dashBoard.mapButtonIsDisplayed(), "Map button not displayed");
+
+        log.info("Go To Map Page");
         MapScreen map = dashBoard.goToMapScreen();
 
-        log.info("Validate Show List Button");
-        //Assert.assertTrue(map.showListIsDisplayed(), "Show List not displayed");
-
         log.info("Validate Category Button");
-       // Assert.assertTrue(map.categoryIsDisplayed(), "Category not displayed");
+        Assert.assertTrue(map.categoryIsDisplayed(), "Category not displayed");
 
-        log.info("Validate Filter Button");
-        //Assert.assertTrue(map.filterIsDisplayed(), "Filter not displayed");
+        CategoriesWidget categoriesScreen = map.openCategoriesList();
+        Assert.assertTrue(categoriesScreen.getNumberOfCategories() > 5);
+
+        log.info("Validate Category Hotel exist in the categories list");
+        Assert.assertTrue(categoriesScreen.existCategoryHotel(), "The category 'Hotel' not exist");
+
     }
 
 }
---
+++ src/test/java/tests/PrivacyAndLegalTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.springframework.context.annotation.Description;
+import org.testng.annotations.Test;
+import util.tests.BaseMobileTest;
+
+public class PrivacyAndLegalTest extends BaseMobileTest {
+
+    @Description(value = "")
+    @Test
+    public void checkLink(){
+
+    }
+}
